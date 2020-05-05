9c6e748e50dfe0aa01f2861627a5cf0d6b4efde8
satyagovi <64708649+satyagovi@users.noreply.github.com>
2020-05-05 15:30:42 -0700

    loginpage

--- frontend/App.js
+++ frontend/App.js
@@ -3,9 +3,11 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./src/screens/LoginScreen";
+import LoginForm from "./src/screens/LoginForm";
+
 const Stack = createStackNavigator();
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  KeyboardAvoidingView
+} from "react-native";
+import LoginForm from "./LoginForm";
 export default class LoginScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>hi</Text>
-      </View>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../images/vaultlogo.png")}
+          />
+          <Text style={styles.title}>VAULT OPS</Text>
+        </View>
+
+        <View style={styles.formContainer}>
+          <LoginForm />
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2c3e50"
+  },
+  logoContainer: {
+    alignItems: "center",
+    flexGrow: 1,
+    justifyContent: "center"
+  },
+  logo: {
+    width: 300,
+    height: 300
+  },
+  title: {
+    color: "black",
+    fontSize: 50,
+    fontWeight: "bold"
+  }
+});

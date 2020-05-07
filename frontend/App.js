f52e3fcc33f33158a3f7681a2cdefa335691e045
satyagovi <64708649+satyagovi@users.noreply.github.com>
2020-05-06 17:01:46 -0700

    AppProduct and login screen

--- frontend/App.js
+++ frontend/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-//import LoginScreen from "./src/screens/LoginScreen";
-//import LoginForm from "./src/screens/LoginForm";
+import LoginScreen from "./src/screens/LoginScreen";
+import LoginForm from "./src/screens/LoginForm";
 import AddProduct from "./src/screens/AddProduct";
 import AddProductform from "./src/screens/AddProductform";
 
@@ -14,6 +14,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Add" component={AddProduct} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );

d74100c81d9ffb43feb729c20e30baf19a7bd669
skamuju <63597158+skamuju@users.noreply.github.com>
2020-05-07 20:45:27 -0400

    Update notflask.py

--- backend/notflask.py
+++ backend/notflask.py
@@ -1,36 +1,36 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
-
 from backend.ASIN import tech_dict, information_amazon
 
 app = Flask(__name__)
+
 
 @app.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
     client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
-    db = client.test
     db = client['Vault_Ops']
-    collection = db['users']
+    collection = db['user_products']
+
     post = {'username': data["username"], 'email': data["email"],
             'password': data["password"], 'devices': []}
 
     collection.insert_one(post)
-    return jsonify({ "success": True })
+    return jsonify({"success": True})
 
-@app.route('/device', methods=['POST'])
+
+@app.route('/addDevice', methods=['POST'])
 def device():
     data = request.get_json()
     client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
-    db = client.test
     db = client['Vault_Ops']
     collection = db['user_products']
     ASIN = tech_dict[data["name"]]
-    url = "http://www.amazon.com/dp/"  + ASIN
-    tech_info = information_amazon(url)
-    post = {'name': tech_info["name"], 'availability': tech_info["availability"], 'price': tech_info['price']}
+    url = "http://www.amazon.com/dp/" + ASIN
+    tech_info,tech_features = information_amazon(url)
+    post = {'name': tech_info[0], 'availability': tech_info[1], 'price': tech_info[2]}
 
     collection.insert_one(post)
-    return jsonify({ "success": True })
+    return jsonify({"success": True})
 
 app.run()

ca2af9764afbedf4c02701b3784ce88b099e8627
skamuju <63597158+skamuju@users.noreply.github.com>
2020-05-09 11:21:44 -0400

    Update to /getProducts

--- backend/notflask.py
+++ backend/notflask.py
@@ -8,7 +8,7 @@ def signup():
     data = request.get_json()
     client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
     db = client['Vault_Ops']
-    collection = db['user_products']
+    collection = db['users']
 
     post = {'username': data["username"], 'email': data["email"],
             'password': data["password"], 'devices': []}
@@ -16,15 +16,26 @@ def signup():
     collection.insert_one(post)
     return jsonify({"success": True})
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
+    db = client['Vault_Ops']
+    collection = db['users']
+    post = {'username': data["username"],'password': data["password"]}
+    if post['username'] and post['password']in collection.find():
+        return jsonify({"success": True})
+    else:
+        return ('Sorry, you are not registered in our user database')
+
+
 @app.route('/addProduct', methods=['POST'])
 def addProduct():
     data = request.get_json()
     client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
     db = client['Vault_Ops']
     collection = db['user_products']
-
     post = {'name': data['name'], 'lifespan':data['lifespan']}
- 
     collection.insert_one(post)
     return jsonify({"success": True})
 
@@ -33,11 +44,19 @@ def getProduct():
     client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
     db = client['Vault_Ops']
     collection = db['user_products']  
-    items = []
+    items = []  
     for doc in collection.find({}):
-        del doc["_id"]
-        items.append(doc)
-    return jsonify({ "data": items })
+            del doc["_id"]
+            items.append(doc)
+    items.reverse()
+    return jsonify({ "data": items}) 
+
+@app.route('/countdb', methods=['GET'])
+def countdb():
+    client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
+    db = client['Vault_Ops']
+    collection = db['user_products']  
+    return str(collection.count())
 
 app.run()
 

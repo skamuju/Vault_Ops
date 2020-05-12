a2407ed754f5f9df1badc846a98f8b63b9771fd9
skamuju <63597158+skamuju@users.noreply.github.com>
2020-05-11 20:45:33 -0400

    /login

--- backend/notflask.py
+++ backend/notflask.py
@@ -16,19 +16,17 @@ def signup():
     collection.insert_one(post)
     return jsonify({"success": True})
 
-#@app.route('/login', methods=['POST'])
-#def login():
-    #data = request.get_json()
-    #client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
-    #db = client['Vault_Ops']
-    #collection = db['users']
-    #post = {'username': data["username"],'password': data["password"]}
-    #return jsonify({"post": db.collection['users']}) #Experimenting
-
-    #if db.collection.find({post["password": data['password']]}): #Problem is here I think you are searching the wrong database? Not sure
-        #return jsonify({"success": True})
-    #else:
-        #return jsonify({"complete failure, put the right password in bruh": True})
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    client = MongoClient("mongodb+srv://s_kamuju:teamfreeze@ssf-kdqtj.mongodb.net/test?retryWrites=true&w=majority")
+    db = client['Vault_Ops']
+    collection = db['users']
+    post = {'username': data["username"],'password': data["password"]}
+    if collection.find_one(post): 
+        return jsonify({"success": True})
+    else:
+        return jsonify({"success": False})
 
 @app.route('/addProduct', methods=['POST'])
 def addProduct():
